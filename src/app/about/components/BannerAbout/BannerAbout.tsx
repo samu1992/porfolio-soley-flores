cd321eace0668014bd4143264cc0e7e71a7c42e1
Samuel Carrizo <samuelcarrizot@gmail.com>
2024-01-02 09:48:33 -0400

    design responsive

--- src/app/about/components/BannerAbout/BannerAbout.tsx
+++ src/app/about/components/BannerAbout/BannerAbout.tsx
@@ -10,16 +10,16 @@ const fraunces = Fraunces({
 export default function BannerAbout() {
     return (
         <>
-            <div className="flex flex-col w-full px-40 py-48 h-96 justify-center iyems-center mb-">
+            <div className="flex flex-col w-full h-48 lg:h-56 py-5 px-8 md:px-24 lg:px-36 xl:px-11 xl:h-96 justify-center">
                 <section>
-                    <h2 className="text-start text-7xl w-full">SOLEY</h2>
+                    <h2 className="text-start text-md md:text-2xl lg:text-3xl xl:text-7xl w-full">SOLEY</h2>
                 </section>
                 <section className="flex flex-row justify-start items-start">
-                    <h2 className="text-start text-7xl mr-2"><strong>FLORES</strong></h2>
+                    <h2 className="text-start text-md md:text-2xl lg:text-3xl xl:text-7xl mr-2"><strong>FLORES</strong></h2>
                     <Svg />
                 </section>
                 <section>
-                    <h2 className="text-start text-7xl">UI/UX &GRAPHIC <span className={`${fraunces.className}`}>designer</span > BASED <span className={`${fraunces.className}`}>in</span> BUENOS AIRES, ARGENTINA.</h2>
+                    <h2 className="text-start text-md md:text-2xl lg:text-3xl xl:text-7xl">UI/UX &GRAPHIC <span className={`${fraunces.className}`}>designer</span > BASED <span className={`${fraunces.className}`}>in</span> BUENOS AIRES, ARGENTINA.</h2>
                 </section>
             </div>
         </>
--- src/app/about/components/History/History.tsx
+++ src/app/about/components/History/History.tsx
@@ -7,18 +7,18 @@ type ImageType = StaticImageData | string;
 
 export default function History() {
     return (
-        <div className="angry-grid relative z-30">
+        <section id="angry-grid" className="flex flex-col px-8 md:px-24 lg:px-36 xl:px-11 relative z-30">
             {aboutInfo.map(({ content, paragraphs }, index) => (
                 <div key={index} id={`item-${index}`}>&nbsp;
                     {typeof content === 'string' ? (
-                        <p className="text-base  mb-10 not-italic text-gray-800">{content}</p>
+                        <p className="paragraph text-base mb-10 not-italic text-gray-800  md:w-3/4">{content}</p>
                     ) : (
                         <Image alt={`image-${index}`} src={content as ImageType} />
                     )}
                     {paragraphs && Array.isArray(paragraphs) && (
                         <div>
                             {paragraphs.map((paragraph, paragraphIndex) => (
-                                <p key={paragraphIndex} className="text-base  mb-10 not-italic text-gray-800">
+                                <p key={paragraphIndex} className=" paragraph text-base mb-10 not-italic text-gray-800">
                                     {Object.values(paragraph).join(' ')}
                                 </p>
                             ))}
@@ -26,6 +26,6 @@ export default function History() {
                     )}
                 </div>
             ))}
-        </div>
+        </section>
     );
 }

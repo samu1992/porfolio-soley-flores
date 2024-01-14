39ccf296d1c2b316109e2207f30dc02c012d1496
Samuel Carrizo <samuelcarrizot@gmail.com>
2024-01-14 14:56:53 -0300

    error link

--- src/app/models/route.model.ts
+++ src/app/models/route.model.ts
@@ -1,4 +1,4 @@
-export type Route = "/" | "https://drive.google.com/drive/u/0/folders/1gR1mP1JUdiruaR0BUypBtbgRKwSj6-U9" | "/about" ;
+export type Route = "/" | "https://drive.google.com/file/d/1umsN3cQo4VdhHZRR4YT3mDwMgNhEU2io/view?usp=drive_link" | "/about" ;
 
 export interface Routes {
   name: string;
